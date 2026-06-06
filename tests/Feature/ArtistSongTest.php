c7c6da9c343c260bbe555a66c28ef974c511d050
escobann <144521899+escobann@users.noreply.github.com>
2026-06-06 15:52:36 +0300

    Add test for user play count in artist songs
    Added a test to verify user play count in song response.

--- tests/Feature/ArtistSongTest.php
+++ tests/Feature/ArtistSongTest.php
@@ -4,9 +4,12 @@ namespace Tests\Feature;
 
 use App\Http\Resources\SongResource;
 use App\Models\Artist;
+use App\Models\Interaction;
 use App\Models\Song;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
+
+use function Tests\create_user;
 
 class ArtistSongTest extends TestCase
 {
@@ -18,5 +21,21 @@ class ArtistSongTest extends TestCase
         Song::factory()->for($artist)->createMany(5);
 
         $this->getAs("api/artists/{$artist->id}/songs")->assertJsonStructure([0 => SongResource::JSON_STRUCTURE]);
+    }
+
+    #[Test]
+    public function indexIncludesUserPlayCount(): void
+    {
+        $user = create_user();
+        $artist = Artist::factory()->createOne();
+        $song = Song::factory()->for($artist)->createOne();
+
+        Interaction::factory()->for($user)->for($song)->createOne(['play_count' => 12]);
+
+        $this->getAs("api/artists/{$artist->id}/songs", $user)
+            ->assertJsonFragment([
+                'id' => $song->id,
+                'play_count' => 12,
+            ]);
     }
 }
